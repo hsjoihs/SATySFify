08d9a333a296b698fcff118d481427cc0f45bf81
hsjoihs <hs.ioling.hs@gmail.com>
2019-09-28 12:35:05 +0900

    make type compatible

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,26 @@ extern {
     fn compile(input: *mut u8);
 }
 
+#[repr(C)]
+pub enum TokenType {
+  ALPHANUMERIC,
+  BACKSLASH_FOLLOWED_BY_ALPHANUMERICS,
+  CARET,
+  UNDERSCORE,
+  ORDINARY_OPERATOR,
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  END,
+}
+
+#[repr(C)]
+pub struct Token {
+  kind: TokenType,
+  string_representation: *const u8
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
