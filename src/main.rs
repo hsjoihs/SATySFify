d343ef183d04933b3e5b26437b6e6e8a7758d18b
hsjoihs <hs.ioling.hs@gmail.com>
2019-09-28 13:26:08 +0900

    Migrate `compile` to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,87 @@
 extern crate libc;
 use std::env;
+use std::ffi::CStr;
 
 #[link(name = "compile")]
-extern {
-    fn compile(input: *mut u8);
+extern "C" {
+    fn get_token(initial: *const u8, ptr_offset: *mut usize) -> Token;
 }
 
 #[repr(C)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 pub enum TokenType {
-  ALPHANUMERIC,
-  BACKSLASH_FOLLOWED_BY_ALPHANUMERICS,
-  CARET,
-  UNDERSCORE,
-  ORDINARY_OPERATOR,
-  LEFT_PAREN,
-  RIGHT_PAREN,
-  LEFT_BRACE,
-  RIGHT_BRACE,
-  END,
+    ALPHANUMERIC,
+    BACKSLASH_FOLLOWED_BY_ALPHANUMERICS,
+    CARET,
+    UNDERSCORE,
+    ORDINARY_OPERATOR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    END,
 }
 
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub struct Token {
-  kind: TokenType,
-  string_representation: *const u8
+    kind: TokenType,
+    string_representation: *const u8,
+}
+
+unsafe fn compile_(input: *mut u8) {
+    let init = input;
+    let mut box_offset: Box<usize> = Box::new(0);
+
+    let mut tokens = Vec::new();
+    loop {
+        let ptr = Box::into_raw(box_offset);
+        let t = get_token(init, ptr);
+        box_offset = Box::from_raw(ptr);
+        tokens.push(t);
+
+        if t.kind == TokenType::END {
+            break;
+        }
+
+        eprintln!(
+            "{}",
+            CStr::from_ptr(t.string_representation as *const i8)
+                .to_str()
+                .unwrap()
+        );
+    }
+
+    print!("{}", "@require: stdjabook\n");
+    print!("{}", "@require: code\n");
+    print!("{}", "@require: itemize\n");
+    print!("{}", "@require: tabular\n");
+    print!("{}", "@require: math\n");
+    print!("{}", "\n");
+    print!("{}", "document (|\n");
+    print!("{}", "  title = {};\n");
+    print!("{}", "  author = {};\n");
+    print!("{}", "  show-title = false;\n");
+    print!("{}", "  show-toc = false;\n");
+    print!("{}", "|) '<\n");
+    print!("{}", "  +section{}<\n");
+    print!("{}", "    +math(${\n");
+
+    for t in &tokens {
+        if t.kind == TokenType::END {
+            break;
+        }
+        println!(
+            "      {}",
+            CStr::from_ptr(t.string_representation as *const i8)
+                .to_str()
+                .unwrap()
+        )
+    }
+
+    print!("{}", "    });\n");
+    print!("{}", "  >\n");
+    print!("{}", ">\n");
 }
 
 fn main() {
@@ -33,5 +91,5 @@ fn main() {
         panic!("Incorrect number of arguments\n");
     }
     let mut input = args[1].clone();
-    unsafe { compile(input.as_mut_ptr()) }
+    unsafe { compile_(input.as_mut_ptr()) }
 }
