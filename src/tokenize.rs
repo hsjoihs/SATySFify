92c5541646b2e8f122bde87a72b0329429c092eb
hsjoihs <hs.ioling.hs@gmail.com>
2019-10-01 01:12:20 +0900

    Convert into iterator

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -20,11 +20,11 @@ pub mod tok {
         pub str_repr: String,
     }
     pub fn to_tokens(input: &[char]) -> Vec<Token> {
-        let mut offset: usize = 0;
+        let mut iter = input.iter().peekable();
 
         let mut tokens = Vec::new();
         loop {
-            match get_token2(input, &mut offset) {
+            match get_token2(&mut iter) {
                 None => {
                     break;
                 }
@@ -37,175 +37,158 @@ pub mod tok {
 
         tokens
     }
-    fn get_token2(initial: &[char], offset: &mut usize) -> Option<Token> {
-        if initial.len() == *offset {
-            return None;
-        }
-
-        if initial[*offset] == ' '
-            || initial[*offset] == '\t'
-            || initial[*offset] == '\n'
-            || initial[*offset] == '\r'
-        {
-            *offset += 1;
-            return get_token2(initial, offset);
-        }
-
-        if initial[*offset] == '+' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "+".to_string(),
-            });
-        } else if initial[*offset] == '*' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "*".to_string(),
-            });
-        } else if initial[*offset] == '(' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::LeftParen,
-                str_repr: "(".to_string(),
-            });
-        } else if initial[*offset] == ')' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::RightParen,
-                str_repr: ")".to_string(),
-            });
-        } else if initial[*offset] == ',' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: ",".to_string(),
-            });
-        } else if initial[*offset] == '.' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: ".".to_string(),
-            });
-        } else if initial[*offset] == '|' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "|".to_string(),
-            });
-        } else if initial[*offset] == '^' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::Caret,
-                str_repr: "^".to_string(),
-            });
-        } else if initial[*offset] == '{' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::LeftBrace,
-                str_repr: "{".to_string(),
-            });
-        } else if initial[*offset] == '}' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::RightBrace,
-                str_repr: "}".to_string(),
-            });
-        } else if initial[*offset] == '-' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "-".to_string(),
-            });
-        } else if initial[*offset] == '<' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "<".to_string(),
-            });
-        } else if initial[*offset] == '>' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: ">".to_string(),
-            });
-        } else if initial[*offset] == '=' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::OrdinaryOperator,
-                str_repr: "=".to_string(),
-            });
-        } else if initial[*offset] == '_' {
-            *offset += 1;
-            return Some(Token {
-                kind: TokenType::Underscore,
-                str_repr: "_".to_string(),
-            });
-        }
-
-        if (initial[*offset] >= 'a' && initial[*offset] <= 'z')
-            || (initial[*offset] >= 'A' && initial[*offset] <= 'Z')
-            || (initial[*offset] >= '0' && initial[*offset] <= '9')
-        {
-            let mut st = String::from("");
-            st.push(initial[*offset] as char);
-
-            *offset += 1;
-
-            return Some(Token {
-                kind: TokenType::Alphanumeric,
-                str_repr: st,
-            });
-        }
-
-        if initial[*offset] == '\\' {
-            if initial.len() == 1 + *offset {
-                eprintln!("Found unexpected end of input after a backslash\n");
-                panic!();
+    fn get_token2(iter: &mut std::iter::Peekable<std::slice::Iter<'_, char>>) -> Option<Token> {
+        match iter.next() {
+            None => {
+                return None;
             }
-            let after_backslash = initial[1 + *offset];
-            if !((after_backslash >= 'a' && after_backslash <= 'z')
-                || (after_backslash >= 'A' && after_backslash <= 'Z'))
-            {
+            Some(&ch) => {
+                if ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' {
+                    return get_token2(iter);
+                }
+
+                if ch == '+' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "+".to_string(),
+                    });
+                } else if ch == '*' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "*".to_string(),
+                    });
+                } else if ch == '(' {
+                    return Some(Token {
+                        kind: TokenType::LeftParen,
+                        str_repr: "(".to_string(),
+                    });
+                } else if ch == ')' {
+                    return Some(Token {
+                        kind: TokenType::RightParen,
+                        str_repr: ")".to_string(),
+                    });
+                } else if ch == ',' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: ",".to_string(),
+                    });
+                } else if ch == '.' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: ".".to_string(),
+                    });
+                } else if ch == '|' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "|".to_string(),
+                    });
+                } else if ch == '^' {
+                    return Some(Token {
+                        kind: TokenType::Caret,
+                        str_repr: "^".to_string(),
+                    });
+                } else if ch == '{' {
+                    return Some(Token {
+                        kind: TokenType::LeftBrace,
+                        str_repr: "{".to_string(),
+                    });
+                } else if ch == '}' {
+                    return Some(Token {
+                        kind: TokenType::RightBrace,
+                        str_repr: "}".to_string(),
+                    });
+                } else if ch == '-' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "-".to_string(),
+                    });
+                } else if ch == '<' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "<".to_string(),
+                    });
+                } else if ch == '>' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: ">".to_string(),
+                    });
+                } else if ch == '=' {
+                    return Some(Token {
+                        kind: TokenType::OrdinaryOperator,
+                        str_repr: "=".to_string(),
+                    });
+                } else if ch == '_' {
+                    return Some(Token {
+                        kind: TokenType::Underscore,
+                        str_repr: "_".to_string(),
+                    });
+                }
+
+                if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')
+                {
+                    let mut st = String::from("");
+                    st.push(ch as char);
+
+                    return Some(Token {
+                        kind: TokenType::Alphanumeric,
+                        str_repr: st,
+                    });
+                }
+
+                if ch == '\\' {
+                    match iter.next() {
+                        None => {
+                            eprintln!("Found unexpected end of input after a backslash\n");
+                            panic!();
+                        }
+                        Some(&after_backslash) => {
+                            // now on 1 + *offset
+
+                            if !((after_backslash >= 'a' && after_backslash <= 'z')
+                                || (after_backslash >= 'A' && after_backslash <= 'Z'))
+                            {
+                                eprintln!(
+                                    "Found unexpected character after a backslash: '{}' ({})\n",
+                                    after_backslash as char, after_backslash as i32
+                                );
+                                panic!();
+                            }
+                            let mut new_st = String::from("");
+                            new_st.push(ch);
+                            new_st.push(after_backslash);
+
+                            loop {
+                                match iter.peek() {
+                                    None => {
+                                        break;
+                                    }
+                                    Some(&&c) => {
+                                        if !((c >= 'a' && c <= 'z')
+                                            || (c >= 'A' && c <= 'Z')
+                                            || (c >= '0' && c <= '9'))
+                                        {
+                                            break;
+                                        }
+                                        new_st.push(c);
+                                        iter.next();
+                                    }
+                                }
+                            }
+
+                            return Some(Token {
+                                kind: TokenType::BackslashFollowedByAlphanumerics,
+                                str_repr: new_st,
+                            });
+                        }
+                    }
+                }
+
                 eprintln!(
-                    "Found unexpected character after a backslash: '{}' ({})\n",
-                    after_backslash as char, after_backslash as i32
+                    "Found unexpected character: '{}' ({})",
+                    ch as char, ch as i32
                 );
                 panic!();
             }
-
-            let mut i = 2;
-
-            loop {
-                if initial.len() == i + *offset {
-                    break;
-                }
-                let c = initial[i + *offset];
-                if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
-                    break;
-                }
-                i += 1;
-            }
-
-            /*
-                identifier: initial[offset + 1] ~ initial[offset + i-1]
-            */
-            let mut new_st = String::from("");
-            for j in 0..i {
-                new_st.push(initial[*offset + j] as char)
-            }
-            *offset += i;
-
-            return Some(Token {
-                kind: TokenType::BackslashFollowedByAlphanumerics,
-                str_repr: new_st,
-            });
         }
-
-        eprintln!(
-            "Found unexpected character: '{}' ({})",
-            initial[*offset] as char, initial[*offset] as i32
-        );
-        panic!();
     }
 }
