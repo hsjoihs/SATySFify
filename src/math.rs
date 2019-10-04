5e4f0fd26b1e2c4ccbbb3411b3374fd7dde0f1c5
hsjoihs <hs.ioling.hs@gmail.com>
2019-10-05 03:27:44 +0900

    Split into math.rs

---
+++ src/math.rs
@@ -0,0 +1,333 @@
+
+pub use crate::math_tokenize::tok;
+use termion::{color, style};
+#[derive(Debug)]
+pub enum Stuff {
+    Simple(tok::Token),
+    Braced(Vec<Stuff>),
+    LeftRightPair(BSLeftKind, Vec<Stuff>, BSRightKind),
+}
+
+#[derive(Copy, Clone)]
+pub enum LeftParenKind {
+    BareLeftParen,
+    BareLeftBrace,
+    BareLeftBracket,
+    BackslashLeft(BSLeftKind),
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum BSLeftKind {
+    LeftParen,
+    LeftBracket,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum BSRightKind {
+    RightParen,
+    RightBracket,
+}
+
+fn to_stuffs(input: Vec<tok::Token>) -> Result<Vec<Stuff>, String> {
+    match to_stuffs_(&mut input.into_iter(), &Vec::new())? {
+        (ans, None) => Ok(ans),
+        _ => unreachable!(),
+    }
+}
+
+fn to_stuffs_(
+    mut iter: &mut std::vec::IntoIter<tok::Token>,
+    paren_stack: &[LeftParenKind],
+) -> Result<(Vec<Stuff>, Option<BSRightKind>), String> {
+    let mut res = Vec::new();
+
+    while let Some(x) = iter.next() {
+        match x.kind {
+            tok::TokenType::Alphanumeric
+            | tok::TokenType::OrdinaryOperator
+            | tok::TokenType::Underscore
+            | tok::TokenType::Caret => {
+                res.push(Stuff::Simple(x));
+            }
+            tok::TokenType::BackslashFollowedByAlphanumerics => {
+                if x.str_repr == "\\left" {
+                    let next_tok = iter
+                        .next()
+                        .ok_or("end of input encountered after `\\left`")?;
+                    match next_tok.kind {
+                        tok::TokenType::LeftParen => {
+                            let mut new_stack = paren_stack.to_owned();
+                            new_stack.push(LeftParenKind::BackslashLeft(BSLeftKind::LeftParen));
+                            let (inner_stuffs, hopefully_something) =
+                                to_stuffs_(&mut iter, &*new_stack)?;
+
+                            res.push(Stuff::LeftRightPair(
+                                BSLeftKind::LeftParen,
+                                inner_stuffs,
+                                hopefully_something.expect("should not happen"),
+                            ));
+                        }
+                        tok::TokenType::LeftBracket => {
+                            let mut new_stack = paren_stack.to_owned();
+                            new_stack.push(LeftParenKind::BackslashLeft(BSLeftKind::LeftBracket));
+                            let (inner_stuffs, hopefully_something) =
+                                to_stuffs_(&mut iter, &*new_stack)?;
+
+                            res.push(Stuff::LeftRightPair(
+                                BSLeftKind::LeftBracket,
+                                inner_stuffs,
+                                hopefully_something.expect("should not happen"),
+                            ));
+                        }
+                        _ => unimplemented!("unimplemented token found after `\\left`"),
+                    }
+                } else if x.str_repr == "\\right" {
+                    let next_tok = iter
+                        .next()
+                        .ok_or("end of input encountered after `\\right`")?;
+                    match next_tok.kind {
+                        tok::TokenType::RightParen => match paren_stack.last() {
+                            None => {
+                                return Err("unmatched `\\right)`".to_string());
+                            }
+                            Some(LeftParenKind::BackslashLeft(_)) => {
+                                return Ok((res, Some(BSRightKind::RightParen)));
+                            }
+                            Some(&x) => {
+                                return Err(format!(
+                                    "`\\right)` encountered before {} was matched",
+                                    x.msg()
+                                ));
+                            }
+                        },
+                        tok::TokenType::RightBracket => match paren_stack.last() {
+                            None => {
+                                return Err("unmatched `\\right]`".to_string());
+                            }
+                            Some(LeftParenKind::BackslashLeft(_)) => {
+                                return Ok((res, Some(BSRightKind::RightBracket)));
+                            }
+                            Some(&x) => {
+                                return Err(format!(
+                                    "`\\right]` encountered before {} was matched",
+                                    x.msg()
+                                ));
+                            }
+                        },
+                        _ => unimplemented!("unimplemented token found after `\\right`"),
+                    }
+                } else {
+                    res.push(Stuff::Simple(x));
+                }
+            }
+
+            tok::TokenType::LeftParen => {
+                let mut new_stack = paren_stack.to_owned();
+                new_stack.push(LeftParenKind::BareLeftParen);
+                let (inner_stuffs, hopefully_none) = to_stuffs_(&mut iter, &*new_stack)?;
+                if hopefully_none.is_some() {
+                    unreachable!();
+                }
+                res.push(Stuff::Simple(tok::Token {
+                    kind: tok::TokenType::BackslashFollowedByAlphanumerics,
+                    str_repr: "\\paren".to_string(),
+                }));
+                res.push(Stuff::Braced(inner_stuffs));
+            }
+            tok::TokenType::LeftBrace => {
+                let mut new_stack = paren_stack.to_owned();
+                new_stack.push(LeftParenKind::BareLeftBrace);
+                let (inner_stuffs, hopefully_none) = to_stuffs_(&mut iter, &*new_stack)?;
+                if hopefully_none.is_some() {
+                    unreachable!();
+                }
+                res.push(Stuff::Braced(inner_stuffs));
+            }
+            tok::TokenType::LeftBracket => {
+                let mut new_stack = paren_stack.to_owned();
+                new_stack.push(LeftParenKind::BareLeftBracket);
+                let (inner_stuffs, hopefully_none) = to_stuffs_(&mut iter, &*new_stack)?;
+                if hopefully_none.is_some() {
+                    unreachable!();
+                }
+                res.push(Stuff::Simple(tok::Token {
+                    kind: tok::TokenType::BackslashFollowedByAlphanumerics,
+                    str_repr: "\\sqbracket".to_string(),
+                }));
+                res.push(Stuff::Braced(inner_stuffs));
+            }
+            tok::TokenType::RightBrace => match paren_stack.last() {
+                None => return Err("unmatched right brace".to_string()),
+                Some(LeftParenKind::BareLeftBrace) => {
+                    return Ok((res, None));
+                }
+                Some(&x) => {
+                    return Err(format!(
+                        "right brace encountered before {} was matched",
+                        x.msg()
+                    ))
+                }
+            },
+            tok::TokenType::RightParen => match paren_stack.last() {
+                None => return Err("unmatched right paren".to_string()),
+                Some(LeftParenKind::BareLeftParen) => {
+                    return Ok((res, None));
+                }
+                Some(&x) => {
+                    return Err(format!(
+                        "right paren encountered before {} was matched",
+                        x.msg()
+                    ))
+                }
+            },
+            tok::TokenType::RightBracket => match paren_stack.last() {
+                None => return Err("unmatched right bracket".to_string()),
+                Some(LeftParenKind::BareLeftBracket) => {
+                    return Ok((res, None));
+                }
+                Some(&x) => {
+                    return Err(format!(
+                        "right bracket encountered before {} was matched",
+                        x.msg()
+                    ))
+                }
+            },
+        };
+    }
+
+    match paren_stack.last() {
+        None => Ok((res, None)),
+        Some(&x) => Err(format!(
+            "end of input encountered before {} was matched",
+            x.msg()
+        )),
+    }
+}
+
+impl LeftParenKind {
+    fn msg(self) -> &'static str {
+        match self {
+            LeftParenKind::BareLeftBrace => "a left brace `{`",
+            LeftParenKind::BackslashLeft(BSLeftKind::LeftParen) => "`\\left(`",
+            LeftParenKind::BackslashLeft(BSLeftKind::LeftBracket) => "`\\left[`",
+            LeftParenKind::BareLeftParen => "a left paren `(`",
+            LeftParenKind::BareLeftBracket => "a left bracket `[`",
+        }
+    }
+}
+
+pub fn print_expr_(stuffs: &[Stuff], indent: usize) {
+    for st in stuffs {
+        match st {
+            Stuff::Simple(t) => {
+                if t.str_repr == "\\le" {
+                    println!("{:indent$}\\leq", "", indent = indent);
+                } else if t.str_repr == "\\dots" {
+                    eprintln!("{}{}`\\dots` detected; converting it into `\\ldots` (you might want to fix this) {}{}", 
+                    color::Fg(color::Red),
+                    style::Bold,
+                    style::Reset,
+                    color::Fg(color::Reset));
+                    println!("{:indent$}\\ldots", "", indent = indent);
+                } else {
+                    println!("{:indent$}{}", "", t.str_repr, indent = indent);
+                }
+            }
+            Stuff::Braced(vec) => {
+                println!("{:indent$}{{", "", indent = indent);
+                print_expr_(vec, indent + 2);
+                println!("{:indent$}}}", "", indent = indent);
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftParen, vec, BSRightKind::RightParen) => {
+                println!("{:indent$}\\paren", "", indent = indent);
+                println!("{:indent$}{{", "", indent = indent);
+                print_expr_(vec, indent + 2);
+                println!("{:indent$}}}", "", indent = indent);
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftBracket, vec, BSRightKind::RightBracket) => {
+                println!("{:indent$}\\sqbracket", "", indent = indent);
+                println!("{:indent$}{{", "", indent = indent);
+                print_expr_(vec, indent + 2);
+                println!("{:indent$}}}", "", indent = indent);
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftParen, vec, BSRightKind::RightBracket) => {
+                println!(
+                    "{:indent$}\\satysfifi-internal-paren-left-sqbracket-right",
+                    "",
+                    indent = indent
+                );
+                println!("{:indent$}{{", "", indent = indent);
+                print_expr_(vec, indent + 2);
+                println!("{:indent$}}}", "", indent = indent);
+            }
+
+            Stuff::LeftRightPair(BSLeftKind::LeftBracket, vec, BSRightKind::RightParen) => {
+                println!(
+                    "{:indent$}\\satysfifi-internal-sqbracket-left-paren-right",
+                    "",
+                    indent = indent
+                );
+                println!("{:indent$}{{", "", indent = indent);
+                print_expr_(vec, indent + 2);
+                println!("{:indent$}}}", "", indent = indent);
+            }
+        }
+    }
+}
+
+pub struct Math {
+    pub stuffs: Vec<Stuff>,
+}
+use std::collections::HashSet;
+pub fn get_what_to_activate_(math: &Math) -> HashSet<String> {
+    get_what_to_activate(&math.stuffs)
+}
+fn get_what_to_activate(stuffs: &[Stuff]) -> HashSet<String> {
+    let mut defs = HashSet::new();
+    for st in stuffs {
+        match st {
+            Stuff::Simple(t) => {
+                defs.insert(t.str_repr.clone());
+            }
+            Stuff::Braced(vec) => {
+                let internal = get_what_to_activate(vec);
+                for k in &internal {
+                    defs.insert(k.to_string());
+                }
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftParen, vec, BSRightKind::RightParen) => {
+                let internal = get_what_to_activate(vec);
+                for k in &internal {
+                    defs.insert(k.to_string());
+                }
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftBracket, vec, BSRightKind::RightBracket) => {
+                let internal = get_what_to_activate(vec);
+                for k in &internal {
+                    defs.insert(k.to_string());
+                }
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftParen, vec, BSRightKind::RightBracket) => {
+                defs.insert("\\satysfifi-internal-paren-left-sqbracket-right".to_string());
+                let internal = get_what_to_activate(vec);
+                for k in &internal {
+                    defs.insert(k.to_string());
+                }
+            }
+            Stuff::LeftRightPair(BSLeftKind::LeftBracket, vec, BSRightKind::RightParen) => {
+                defs.insert("\\satysfifi-internal-sqbracket-left-paren-right".to_string());
+                let internal = get_what_to_activate(vec);
+                for k in &internal {
+                    defs.insert(k.to_string());
+                }
+            }
+        }
+    }
+    eprintln!("{:?}", defs);
+    defs
+}
+
+pub fn to_math(input: Vec<tok::Token>) -> Result<Math, String> {
+    let stuffs = to_stuffs(input)?;
+    Ok(Math { stuffs })
+}
